df62dc017672e659d4c3170ccc4c76d041ad607d
Mohammad Mazraeshahi <m.mazraeshahi@gmail.com>
2024-06-05 11:14:29 +0330

    Spring boot init

---
+++ src/test/java/com/nettube/search/image/downloader/ImageDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nettube.search.image.downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
